67ee9c573fe246296e4cfab96582308fa80bd39f
Santiago-1505 <santiagovillegas853@gmail.com>
2025-04-15 11:20:23 -0500

    Se agregó el modelo de usuario y la conexión con MongoDB. También se implementó el registro de usuarios en la base de datos.

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,24 @@
+import User from '../models/user.model.js';
+
+export const register = async (req, res) => {
+    const { username, email, password } = req.body;
+   
+    try {
+        const newUser = new User({
+        username,
+        email,
+        password
+    })
+
+    const userSaved = await newUser.save();
+    res.json(userSaved);
+    
+    } catch (error) {
+        console.error(error);
+        
+    }
+
+    
+}
+
+export const login = (req, res) => res.send('login');
--- src/db.js
+++ src/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost/tidytaskdb')  
+        await mongoose.connect('mongodb://localhost/tidytaskdb') 
+        console.log('DB connected')
     } catch (error) {
         console.error(error);
     }
