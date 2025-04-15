a77b2abbea36e7b3c70ba776d023b5b567d97a2f
Santiago-1505 <santiagovillegas853@gmail.com>
2025-04-15 13:00:01 -0500

    Se Se generó el token JWT para los usuarios

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,24 +1,34 @@
 import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import { createdAccessToken } from '../libs/jwt.js';
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body;
-   
-    try {
-        const newUser = new User({
-        username,
-        email,
-        password
-    })
 
-    const userSaved = await newUser.save();
-    res.json(userSaved);
-    
+    try {
+        const passwordHash = await bcrypt.hash(password, 10)
+
+        const newUser = new User({
+            username,
+            email,
+            password: passwordHash,
+        });
+
+        const userSaved = await newUser.save();
+        const token = await createdAccessToken({ id: userSaved._id });
+        res.cookie('token', token)
+        res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createdAt: userSaved.createdAt,
+            updatedAt: userSaved.updatedAt,
+        });
     } catch (error) {
-        console.error(error);
-        
+        res.status(500).json({ message: error.message });
+
     }
 
-    
-}
+};
 
 export const login = (req, res) => res.send('login');
