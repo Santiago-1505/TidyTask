17c9db0344fc719dbd424cdc93d9ec0e3d78632e
Santiago-1505 <santiagovillegas853@gmail.com>
2025-06-09 21:53:54 -0500

    cambios en Navbar antes de cambiar de rama

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -39,7 +39,7 @@ function Navbar() {
               <li className="text-sm text-zinc-300">
                 Bienvenido,{" "}
                 <Link
-                  to="/profile"  // Link al perfil del usuario
+                  to="/profile"  
                   className="text-white font-semibold hover:text-indigo-400 transition-colors"
                 >
                   {user.username}
